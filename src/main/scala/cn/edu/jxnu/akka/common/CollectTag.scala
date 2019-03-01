fe44c0108a955b2ea874f97fc3e0acbce0ff1484
梦境迷离 <568845948@qq.com>
2019-03-02 00:09:19 +0800

    tag提取

---
+++ src/main/scala/cn/edu/jxnu/akka/common/CollectTag.scala
@@ -0,0 +1,17 @@
+package cn.edu.jxnu.akka.common
+
+/**
+ * 需要抓取的tag
+ *
+ * 目前仅做粗略抓取
+ */
+object CollectTag {
+
+    val Tag_A = "a"
+    val Tag_Title = "title"
+    val Tag_Article = "article"
+    val Tag_Img = "img"
+    val Tag_Pre = "pre"
+    val Tag_Body = "body"
+
+}
